ea34f80e1b2aa4ca9c4df138ad216695a8897b8e
Pedro Henrique <130615490+henriqueez7@users.noreply.github.com>
2023-06-19 23:10:52 -0300

    commit inicial

---
+++ js/maquinadeescrever.js
@@ -0,0 +1,13 @@
+
+function ativaletra(elemento){
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((letra, i)=>{
+        setTimeout(()=>{
+            elemento.innerHTML += letra; 
+        }, 75 * i);
+    })
+}
+
+const titulo = document.querySelector('.meu-nome2');
+ativaletra(titulo);
